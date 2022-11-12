19ca78f5581a6fac54284ab878d14741f869e1a8
Bjorn <BjornWilliams@users.noreply.github.com>
2022-11-12 11:35:15 -0500

    Updated the project names and copyright
    Update the project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'VersaTul'
+copyright = '2022, VersaTul'
+author = 'Bjorn'
 
 release = '0.1'
 version = '0.1.0'
